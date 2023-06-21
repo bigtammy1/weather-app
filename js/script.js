e2fee3005eba54354e930312b60ab85a0ad65e32
bigtammy1 <bigtammy1@gmail.com>
2023-06-21 16:43:36 +0000

    JS complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
+let searchInp = document.querySelector('.weather__search');
+let city = document.querySelector('.weather__city');
+let day = document.querySelector('.weather__day');
+let humidity = document.querySelector('.weather__indicator--humidity>.value');
+let wind = document.querySelector('.weather__indicator--wind>.value');
+let pressure = document.querySelector('.weather__indicator--pressure>.value');
+let image = document.querySelector('.weather__image');
+let temperature = document.querySelector('.weather__temperature>.value');
 let weatherAPIKey = 'eccde04f4cfb27bcc9d5b7f2f010833d';
-let weatherBasEndpoint = 'https://api.openweathermap.org/data/2.5/weather?appid=' + weatherAPIKey;
+let weatherBasEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
+let cityBaseEndpoint = 'https://api.teleport.org/api/cities/?search=';
 
 let getWeatherByCityName = async (city) => {
     let endpoint = weatherBasEndpoint + '&q=' + city;
     let response = await fetch(endpoint);
     let weather = await response.json();
-
-    console.log(weather);
+    return weather;
 }
 
-getWeatherByCityName('New York');
+searchInp.addEventListener('keydown', async (e) => {
+    if(e.keyCode === 13) {
+        let weather = await getWeatherByCityName(searchInp.value);
+        updateCurrentWeather(weather);
+    }
+})
+
+let updateCurrentWeather = (data) => {
+    console.log(data)
+    city.textContent = data.name + ', ' + data.sys.country;
+    day.textContent = dayOfWeek();
+    humidity.textContent = data.main.humidity;
+    pressure.textContent = data.main.pressure;
+    let windDirection;
+    let deg = data.wind.deg;
+    if(deg > 45 && deg <= 135) {
+        windDirection = 'East';
+    } else if(deg > 135 && deg <= 225) {
+        windDirection = 'South';
+    } else if(deg > 225 && deg <= 315) {
+        windDirection = 'West';
+    } else {
+        windDirection = 'North';
+    }
+    wind.textContent = windDirection + ', ' + data.wind.speed;
+    temperature.textContent = data.main.temp > 0 ? 
+                                ' ' + Math.round(data.main.temp) : 
+                                Math.round(data.main.temp);
+}
+let dayOfWeek = () => {
+    return new Date().toLocaleDateString('en-EN', {'weekday': 'long'});
+}
